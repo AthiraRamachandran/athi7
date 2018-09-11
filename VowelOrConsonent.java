4f7c063b0867098dd225f4c2137df0a70da82098
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2018-09-11 17:56:49 +0530

    Update VowelOrConsonent.java

--- VowelOrConsonent.java
+++ VowelOrConsonent.java
@@ -1,13 +1,18 @@
-public class VowelConsonant {
-
-    public static void main(String[] args) {
-
-        char ch = 'i';
-
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " is vowel");
-        else
-            System.out.println(ch + " is consonant");
-
-    }
+class VowelOrConsonant 
+{
+	public static void main(String[ ] arg)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=sc.next( ).charAt(0);	 		
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+	System.out.println("Entered character "+ch+" is  Vowel"); 
+	}
+	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonant");
+	      else
+		System.out.println("Not an alphabet");		
+	}
 }
