17de88fa4ce71a97108b8b3d3de9de38d0d1d07a
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-01-27 11:39:49 +0530

    Create EvenNumberByInterval.java

---
+++ EvenNumberByInterval.java
@@ -0,0 +1,14 @@
+package students;
+import java.util.*;
+public class EvenNumberByInterval {
+public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	for(int i=a;i<b;i++) {
+		if(i%2 == 0) {
+			System.out.println(i);
+		}
+	}
+}
+}
