c7ee96195731f19691c624a0e2f7eab8afe5c791
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-02-21 18:56:53 +0530

    Create StringAppendFullStop.java

---
+++ StringAppendFullStop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringAppendFullStop
+{
+   public static void main(String args[])
+   {
+       Scanner sc=new Scanner(System.in);
+       String S=sc.next();
+       StringBuffer a= new StringBuffer(S);
+       System.out.println(S+".");
+       
+   }
+}
