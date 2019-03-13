e181673bbe7ca0a7ae89db91360ddd90edbee6df
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-03-13 11:26:26 +0530

    Create FirstFiveMultiples.java

---
+++ FirstFiveMultiples.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        int a=0;
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  for(int i=1;i<=n;i++){
+      a=n*i;
+        System.out.print(a+" ");
+
+  }
+    }
+}
