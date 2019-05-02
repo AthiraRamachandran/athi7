40a7ed28526d99bd20d31c278eebea02882de272
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-05-02 12:38:38 +0530

    Create TimeInMinutes.java

---
+++ TimeInMinutes.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int time=sc.nextInt();
+        int hours=time/2;
+        int minutes=time%2;
+        System.out.println(hours+" "+minutes);
+        
+    }
+} 
