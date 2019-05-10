63688d29481c656336db8afc9b5fbe4138c27031
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-05-10 14:15:17 +0530

    Create Composite.java

---
+++ Composite.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Composite {
+    public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int count=0;
+       for(int i=a;i>=1;i--){
+           if(a%i==0){
+               count++;
+           }
+       }
+       if(count!=2){
+           System.out.println("yes");
+       }else{
+           System.out.println("no");
+       }
+    }
+}
