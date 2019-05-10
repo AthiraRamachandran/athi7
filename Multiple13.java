363c3c7ea794ed2cf388780df9a33c8b0ebe8d3a
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-05-10 14:28:13 +0530

    Update and rename MultipleOf13.java to Multiple13.java

--- Multiple13.java
+++ Multiple13.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class MultipleOf13 {
+public class Multiple13 {
     public static void main(String args[]) {
        Scanner sc=new Scanner(System.in);
        int a=sc.nextInt();
