fa74d83d5410c1ad33627f15776816e57f7b233e
AthiraRamachandran <42938309+AthiraRamachandran@users.noreply.github.com>
2019-06-12 11:25:23 +0530

    Create Swapping.java

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+package prp;
+import java.util.*;
+public class Swapping {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int temp;
+		temp=n1;
+		n1=n2;
+		n2=temp;
+		System.out.println(n1+" "+n2);
+	}
+
+}
